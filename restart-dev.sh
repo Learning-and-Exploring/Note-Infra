c37285b06095ad510e20456aa62409988e8ce47c
Oeun Nuphea <nupheaoeun@gmail.com>
2026-03-14 12:31:08 +0700

    11

--- restart-dev.sh
+++ restart-dev.sh
@@ -4,7 +4,7 @@
 docker compose -f docker-compose.dev.yml --env-file .env down
 
 # Remove old image
-docker rmi ghcr.io/oeun-nuphea/note-api:latest
+# docker rmi ghcr.io/oeun-nuphea/note-api:latest
 
 # Rebuild and start containers in detached mode
-docker compose -f docker-compose.dev.yml --env-file .env up -d
+docker compose -f docker-compose.dev.yml --env-file .env up -d --build
